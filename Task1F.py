af08d93df7e0d53cfb72ab5515e2141a1877247f
Oliver Sier <ojss2@cam.ac.uk>
2021-01-12 20:55:46 +0000

    Removed Duplicate line

--- Task1F.py
+++ Task1F.py
@@ -1,6 +1,5 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import inconsistent_typical_range_stations
-from floodsystem.stationdata import build_station_list
 
 x = [i.name for i in inconsistent_typical_range_stations(build_station_list())]
 x.sort()
