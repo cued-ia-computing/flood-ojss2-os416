1cae50e306c4920f5a9968f7c6c47fe49675fcdb
Oliver Sier <ojss2@cam.ac.uk>
2021-01-25 18:01:17 +0000

    Fixed flake8 failures

--- test_geo.py
+++ test_geo.py
@@ -3,11 +3,11 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.station import MonitoringStation
 
+
 def test_stations_by_distance():
 
     stations = []
-    n = 0
-    coord_list = [(52.2, 0.11),(53.79648, -1.54785),(50.718395, -1.883377)]
+    coord_list = [(52.2, 0.11), (53.79648, -1.54785), (50.718395, -1.883377)]
     p = (51.509865, -0.118092)
     for i in range(len(coord_list)):
         # Create a station
@@ -20,6 +20,6 @@ def test_stations_by_distance():
         town = "My Town" + str(i)
         stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
 
-    assert stations_by_distance(stations, p) == [(stations[0], 78.32212827453219), (stations[2], 151.419460832818), (stations[1], 271.9227491652322)]
-    
-
+    assert stations_by_distance(stations, p) == [(stations[0], 78.32212827453219),
+                                                (stations[2], 151.419460832818),
+                                                (stations[1], 271.9227491652322)]
