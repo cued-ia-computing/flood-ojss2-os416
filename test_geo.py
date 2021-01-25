8ed65b79ce3e860abccdcedd2cb703f15c4b88e6
Oskarie7 <os416@cam.ac.uk>
2021-01-25 19:34:14 +0000

    test_for_stations_by_river

--- test_geo.py
+++ test_geo.py
@@ -1,6 +1,7 @@
 """Unit test for the geo module."""
 
-from floodsystem.geo import rivers_with_station, stations_by_distance, stations_within_radius
+from collections import defaultdict
+from floodsystem.geo import rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius
 from floodsystem.station import MonitoringStation
 
 
@@ -61,8 +62,35 @@ def test_rivers_with_station():
         stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
 
     assert rivers_with_station(stations) == {stations[0].river, stations[1].river, stations[2].river}
+
+
+
+
+
+
+
+
+def test_stations_by_river():
+
+    stations = []
+    Riv = defaultdict(list)
+    coord_list = [(52.2, 0.11), (53.79648, -1.54785), (50.718395, -1.883377)]
+    for i in range(len(coord_list)):
+        # Create a station
+        s_id = "test-s-id n"
+        m_id = "test-m-id n"
+        label = "some station n"
+        coord = coord_list[i]
+        trange = (-2.3, 3.4445)
+        river = "River" + str(i)
+        town = "My Town" + str(i)
+        stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
+        Riv[stations[i].river].append(stations[i])
+
+    assert Riv == (stations_by_river(stations))
+
+
+
+     
+     
     
-
-    
-
-
