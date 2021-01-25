712d06532e7eaab590b8114bb3d7d8f026375fd6
Oskarie7 <os416@cam.ac.uk>
2021-01-25 16:48:22 +0000

    Importing for test_geo

--- test_geo.py
+++ test_geo.py
@@ -1,3 +1,7 @@
 """Unit test for the geo module."""
 
 import floodsystem.geo
+
+from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+from collections import defaultdict 
