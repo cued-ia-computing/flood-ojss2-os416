3b0370978f67ce057d7fd8ad56ace500a4aa2854
Oskarie7 <os416@cam.ac.uk>
2021-01-25 20:09:47 +0000

    test_rivers_by_station_number

--- test_geo.py
+++ test_geo.py
@@ -1,7 +1,7 @@
 """Unit test for the geo module."""
 
 from collections import defaultdict
-from floodsystem.geo import rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius
+from floodsystem.geo import rivers_by_station_number, rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius
 from floodsystem.station import MonitoringStation
 
 
@@ -91,6 +91,34 @@ def test_stations_by_river():
 
 
 
-     
-     
-    
+
+
+
+
+def test_rivers_by_station_number():
+
+    stations = []
+    coord_list = [(52.2, 0.11), (53.79648, -1.54785), (50.718395, -1.883377)]
+    coord_list2 = [(52.7, 0.67), (51.0006, -1.0), (50.718395, -0.9857)]
+    for i in range(len(coord_list)):
+        # Create a station
+        s_id = "test-s-id n"
+        m_id = "test-m-id n"
+        label = "some station n"
+        coord = coord_list[i]
+        trange = (-2.3, 3.4445)
+        river = "River" + str(i)
+        town = "My Town" + str(i)
+        stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
+    for i in range(len(coord_list2)):
+        # Create a station
+        s_id = "test-s-id n"
+        m_id = "test-m-id n"
+        label = "some station n"
+        coord = coord_list2[i]
+        trange = (-2.3, 3.4445)
+        river = "River" + str(i**2)
+        town = "My Town" + str(i)
+        stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
+
+    assert rivers_by_station_number(stations, 2) == [('River0', 2), ('River1', 2)]
