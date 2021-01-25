9525a908301a25f3a92e417148c65d428e96b3e7
Oskarie7 <os416@cam.ac.uk>
2021-01-25 18:23:42 +0000

    test_stations_within_radius

--- test_geo.py
+++ test_geo.py
@@ -1,6 +1,6 @@
 """Unit test for the geo module."""
 
-from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_by_distance, stations_within_radius
 from floodsystem.station import MonitoringStation
 
 
@@ -23,3 +23,23 @@ def test_stations_by_distance():
     assert stations_by_distance(stations, p) == [(stations[0], 78.32212827453219),
                                                 (stations[2], 151.419460832818),
                                                 (stations[1], 271.9227491652322)]
+
+
+def test_stations_within_radius():
+
+    stations = []
+    coord_list = [(52.2, 0.11), (53.79648, -1.54785), (50.718395, -1.883377)]
+    p = (51.509865, -0.118092)
+    r = 160
+    for i in range(len(coord_list)):
+        # Create a station
+        s_id = "test-s-id n"
+        m_id = "test-m-id n"
+        label = "some station n"
+        coord = coord_list[i]
+        trange = (-2.3, 3.4445)
+        river = "River" + str(i)
+        town = "My Town" + str(i)
+        stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
+
+    assert stations_within_radius(stations, p, 160) == [stations[0], stations[2]]
