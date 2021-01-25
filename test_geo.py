8b51bd86bfc254156df13afec4c7fa73290a401c
Oliver Sier <ojss2@cam.ac.uk>
2021-01-25 17:50:34 +0000

    Corrected and fixed stations_by_distance test

--- test_geo.py
+++ test_geo.py
@@ -1,32 +1,25 @@
 """Unit test for the geo module."""
 
-import floodsystem.geo
-
-from .utils import sorted_by_key  # noqa
-from haversine import haversine, Unit
-from collections import defaultdict
-from stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+from floodsystem.station import MonitoringStation
 
 def test_stations_by_distance():
 
     stations = []
     n = 0
-
-    for n in range 3:
-                    # Create a station
+    coord_list = [(52.2, 0.11),(53.79648, -1.54785),(50.718395, -1.883377)]
+    p = (51.509865, -0.118092)
+    for i in range(len(coord_list)):
+        # Create a station
         s_id = "test-s-id n"
         m_id = "test-m-id n"
         label = "some station n"
-        coord = (-2.0*n, 4.0)
-        trange = (-2.3*n, 3.4445)
-        river = "River" + str(n)
-        town = "My Town" + str(n)
+        coord = coord_list[i]
+        trange = (-2.3, 3.4445)
+        river = "River" + str(i)
+        town = "My Town" + str(i)
         stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
 
-    return stations
-
-print(test_stations_by_distance())
-
-
+    assert stations_by_distance(stations, p) == [(stations[0], 78.32212827453219), (stations[2], 151.419460832818), (stations[1], 271.9227491652322)]
     
 
