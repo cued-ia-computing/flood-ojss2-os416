e91b17f9b78619e128a44b89d56fc13a2892a7da
Oskarie7 <os416@cam.ac.uk>
2021-01-25 17:34:37 +0000

    test_geo

--- test_geo.py
+++ test_geo.py
@@ -4,4 +4,29 @@ import floodsystem.geo
 
 from .utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
-from collections import defaultdict 
+from collections import defaultdict
+from stationdata import build_station_list
+
+def test_stations_by_distance():
+
+    stations = []
+    n = 0
+
+    for n in range 3:
+                    # Create a station
+        s_id = "test-s-id n"
+        m_id = "test-m-id n"
+        label = "some station n"
+        coord = (-2.0*n, 4.0)
+        trange = (-2.3*n, 3.4445)
+        river = "River" + str(n)
+        town = "My Town" + str(n)
+        stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
+
+    return stations
+
+print(test_stations_by_distance())
+
+
+    
+
