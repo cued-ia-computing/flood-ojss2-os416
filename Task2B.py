f68f3106d194c8ec0f3bdf0322f76c97cc0c884e
Oskarie7 <os416@cam.ac.uk>
2021-02-06 18:48:48 +0000

    2B

---
+++ Task2B.py
@@ -0,0 +1,15 @@
+
+
+
+from floodsystem.flood import stations_level_over_threshold
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+
+stations = build_station_list()
+update_water_levels(stations)
+
+stations_over_threshold = stations_level_over_threshold(stations, 0.8) 
+
+for i in stations_over_threshold:
+    print(i[0].name, i[1])
+
