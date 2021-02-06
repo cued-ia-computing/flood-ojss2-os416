4961284c785c95f6126a833f96fa3bdcbe0ef9a6
Oskarie7 <os416@cam.ac.uk>
2021-02-06 19:48:20 +0000

    station test edits

--- test_station.py
+++ test_station.py
@@ -79,4 +79,3 @@ def test_relative_water_level():
     assert stations[0].relative_water_level() == 0.5
     assert stations[1].relative_water_level() == None
     assert stations[2].relative_water_level() == None
-    
