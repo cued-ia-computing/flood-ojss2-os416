41863a968df99871bd26c3165220ec0027bcb47a
Oliver Sier <ojss2@cam.ac.uk>
2021-02-06 14:10:33 +0000

    2E and 2F

--- Task2F.py
+++ Task2F.py
@@ -1,15 +1,14 @@
-from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.plot import plot_water_levels
+#from floodsystem.flood import stations_highest_rel_level
+from floodsystem.plot import plot_many_water_levels
 from floodsystem.stationdata import build_station_list
-from floodsystem.flood import stations_highest_rel_level
-from datetime import datetime, timedelta
+from floodsystem.datafetcher import fetch_measure_levels
+from datetime import timedelta
 
-
-
-dt = 10
+dt = 2
 N = 5
+p = 4
 
-stations = stations_highest_rel_level(build_station_list(), N):
-for station in stations:
-    dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days=dt))
-    plot_water_levels(station, dates, levels)
+#stations = stations_highest_rel_level(build_station_list()[6], N)
+stations = build_station_list()[6:8]
+
+plot_many_water_levels(stations, dt=timedelta(days=dt), polyfit = p)
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -3,17 +3,48 @@ plotting data.
 
 """
 
-import matplotlib.pyplot as plt
+from .datafetcher import fetch_measure_levels
+from .analysis import polyfit
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import column
+from bokeh.models import Span
+import pandas as pd
+import numpy as np
+from matplotlib.dates import date2num
 
-def plot_water_levels(station, dates, levels):
-    plt.plot(dates, levels)
-    plt.axhline(y = station.typical_range[0], color = 'g', linestyle = '-')
-    plt.axhline(y = station.typical_range[1], color = 'r', linestyle = '-')
+def plot_water_level(station, dates, levels, show = True):
+    df = pd.DataFrame({'date': dates, 'level': levels})
+    output_file("datetime.html")
+    min_level, max_level = station.typical_range
+    range = max_level - min_level
+    # create a new plot with a datetime axis type
+    p = figure(plot_width=800, plot_height=500, x_axis_type="datetime", y_range=[min_level - range * 0.1, max_level + range * 0.1])
 
-    plt.xlabel('date')
-    plt.ylabel('water level (m)')
-    plt.xticks(rotation=45)
-    plt.title(station.name)
+    p.line(df['date'], df['level'], color='navy', alpha=0.5)
+    low_range = Span(location=min_level,dimension='width', line_color='green',line_width=2)
+    high_range = Span(location=max_level,dimension='width', line_color='red',line_width=2)
+    p.renderers.extend([low_range, high_range])
+    if show:
+        show(p)
+    return p
 
-    plt.tight_layout()
-    plt.show()
+def plot_many_water_levels(stations, dt, polyfit = None):
+    plots = []
+    for station in stations:
+        dates, levels = fetch_measure_levels(station.measure_id, dt)
+        if polyfit != None:
+            p = plot_water_level_with_fit(station, dates, levels, polyfit, show = False)
+        else:
+            p = plot_water_level(station, dates, levels , show = False)
+        plots.append(p)
+    show(column(plots))
+
+def plot_water_level_with_fit(station, dates, levels, p, show = True):
+    poly, d0 = polyfit(dates, levels, p)
+    p = plot_water_level(station, dates, levels, show = False)
+    t = date2num(np.array(dates))
+    df = pd.DataFrame({'t': dates, 'level': poly(t - d0)})
+    p.line(df['t'], df['level'], color='orange', alpha=1)
+    if show:
+        show(p)
+    return p
